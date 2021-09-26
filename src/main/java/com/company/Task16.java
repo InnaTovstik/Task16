374358b23e4618ffcf8938143bb9b8b1b0a97e45
InnaTovstik <kitimail69@gmail.com>
2021-09-26 10:52:59 +0300

    Reformat code

--- src/main/java/com/company/Task16.java
+++ src/main/java/com/company/Task16.java
@@ -2,21 +2,20 @@ package com.company;
 
 import org.apache.commons.math3.primes.Primes;
 
-    public class Task16 {
-        public static void main(String[] args) {
-            System.out.println("Hello, Primes");
-             for (String arg : args) {
-                try {
-                    int number = Integer.parseInt(arg);
-                    if (number == 1 || Primes.isPrime(number)){
-                        System.out.println(arg + " - prime");
-                    }
-                    else {
-                        System.out.println(arg + " - not prime");
-                    }
-                } catch (NumberFormatException e) {
-                    System.out.println(arg + " - NOT A NUMBER");
+public class Task16 {
+    public static void main(String[] args) {
+        System.out.println("Hello, Primes");
+        for (String arg : args) {
+            try {
+                int number = Integer.parseInt(arg);
+                if (number == 1 || Primes.isPrime(number)) {
+                    System.out.println(arg + " - prime");
+                } else {
+                    System.out.println(arg + " - not prime");
                 }
+            } catch (NumberFormatException e) {
+                System.out.println(arg + " - NOT A NUMBER");
             }
         }
     }
+}
